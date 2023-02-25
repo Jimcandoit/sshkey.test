12e4464f7511543ad30b1bb2e073ad3eb44ccc41
JIMCOMPUTER\User <e4871752@gmail.com>
2023-02-25 21:42:37 +0800

    test

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,28 @@
 const { defineConfig } = require("cypress");
-
+const test_env = process.env.NODE_ENV || 'BOSS';
+const env = require(`./${test_env}.json`);
+// const GLOBAL_OBJECT = {};
 module.exports = defineConfig({
   e2e: {
+    viewportWidth : 1550,
+    viewportHeight : 900,
+    baseUrl : env.baseUrl,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+      // on('task', {
+      //   // deconstruct the individual properties
+      //   set({ key, value }) {
+          
+      //     GLOBAL_OBJECT[key] = value;
+      //     return GLOBAL_OBJECT[key];
+      //   },
+      //   get(key) {
+      //     return GLOBAL_OBJECT[key];
+      //   },
+      //   remove(key) {
+      //     return delete GLOBAL_OBJECT[key];
+      //   },
+      // })
+    }
   },
+  env
 });
--- cypress/E2E/fixtures/BOSS.cy.js
+++ cypress/E2E/fixtures/BOSS.cy.js
@@ -1,11 +1,14 @@
-describe('BOSS' , function () {
-  
-  const baseUrl = 'https://www.stockboss.io'
+describe('STOCK BOSS', () => {
+
+  const stockNumber = 'TSM'
+  const baseUrl = Cypress.env('baseUrl')
 
   it ('Search Stock' , function () {
+    
+    cy.visit(baseUrl + stockNumber)
 
-    cy.visit(baseUrl)
-
+    cy.get('#keyratio-table > tbody > :nth-child(7) > :nth-child(28)').invoke('text').should('contain', 6.51)
+    cy.get('#keyratio-table > tbody > :nth-child(12) > :nth-child(28)').invoke('text').should('contain', 1.83)
 
 
 
